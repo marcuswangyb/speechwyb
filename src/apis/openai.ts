582c9673faf48268158b2ec7d2fa66f2fb1ad0de
marcus <marcuswangyb@gmail.com>
2023-10-23 15:09:25 +0800

    openaiHost

--- src/apis/openai.ts
+++ src/apis/openai.ts
@@ -8,6 +8,8 @@ export default async function sendRequest(
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      // direct mode
+      Authorization: 'Bearer ' + openaiApiKey,
     },
     body: JSON.stringify({
       model: 'gpt-3.5-turbo',
@@ -17,13 +19,29 @@ export default async function sendRequest(
   console.log("json:");
   console.log(JSON.stringify({model: 'gpt-3.5-turbo',messages: messages}));
 
+  // backend mode
   const chatHostAddress = import.meta.env.DEV?
     'remote':
     'https://www.wybstudio.net';
+    
 
   console.log("mode:",import.meta.env.MODE)
 
   fetch(chatHostAddress + '/api/speech/chat', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      callback(data);
+    })
+    .catch(err => {
+      return err;
+    });
+  
+  // direct mode 
+  const openaiHostAddress = openaiHost || 'api.openai.com';
+
+  console.log("key:",openaiApiKey);
+
+  fetch('https://' + openaiHostAddress + '/v1/chat/completions', requestOptions)
     .then(response => response.json())
     .then(data => {
       callback(data);
